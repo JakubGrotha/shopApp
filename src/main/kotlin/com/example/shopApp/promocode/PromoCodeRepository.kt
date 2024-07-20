60ca94c406008bc88fb2701f610fbf27cbdc0b0a
Jakub Grotha <jakubjewstafiew@gmail.com>
2024-07-21 00:27:47 +0200

    Bump gradle version to 8.8 + small fixes

--- src/main/kotlin/com/example/shopApp/promocode/PromoCodeRepository.kt
+++ src/main/kotlin/com/example/shopApp/promocode/PromoCodeRepository.kt
@@ -2,7 +2,9 @@ package com.example.shopApp.promocode
 
 import com.example.shopApp.promocode.model.PromoCode
 import org.springframework.data.mongodb.repository.MongoRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface PromoCodeRepository: MongoRepository<PromoCode, String> {
 
     fun findByCode(code: String): PromoCode?
