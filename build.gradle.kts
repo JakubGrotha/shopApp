8d83eac26858b529dc7928e56aca23b6c1b1ec5e
Jakub Grotha <jakubjewstafiew@gmail.com>
2024-07-21 00:28:28 +0200

    Bump gradle version to 8.9

--- build.gradle.kts
+++ build.gradle.kts
@@ -56,6 +56,8 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.security:spring-security-test")
     testImplementation("io.projectreactor:reactor-test")
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.withType<KotlinCompile> {
