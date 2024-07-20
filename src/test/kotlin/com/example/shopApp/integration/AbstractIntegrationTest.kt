8dd131a7d7db3c06a86ca01e71fad49aa0d58fb5
Jakub Grotha <jakubjewstafiew@gmail.com>
2024-07-21 00:28:09 +0200

    Remove unused annotation

--- src/test/kotlin/com/example/shopApp/integration/AbstractIntegrationTest.kt
+++ src/test/kotlin/com/example/shopApp/integration/AbstractIntegrationTest.kt
@@ -3,7 +3,6 @@ package com.example.shopApp.integration
 import com.example.shopApp.product.ProductRepository
 import com.example.shopApp.promocode.PromoCodeRepository
 import com.fasterxml.jackson.databind.ObjectMapper
-import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.BeforeEach
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
